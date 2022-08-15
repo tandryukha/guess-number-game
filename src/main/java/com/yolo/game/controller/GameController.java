b6198c142c066f1ca0809bd4e42e9812a3e8a75b
ataran <andrew@1worldonline.com>
2022-08-15 19:30:01 +0300

    Game Controller

--- src/main/java/com/yolo/game/controller/GameController.java
+++ src/main/java/com/yolo/game/controller/GameController.java
@@ -6,18 +6,22 @@ import com.yolo.game.engine.Player;
 import com.yolo.game.engine.PlayerNotification;
 import com.yolo.game.event.EventAdapter;
 import com.yolo.game.event.PlayerEvent;
+import jdk.internal.access.JavaIOFileDescriptorAccess;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.List;
-import java.util.Optional;
+import java.io.IOException;
+import java.util.*;
 
+@Slf4j
 public class GameController extends TextWebSocketHandler implements GameObserver {
     private final GameEngine gameEngine;
 
     private final EventAdapter eventAdapter;
+    private final Map<String, WebSocketSession> sessions = Collections.synchronizedMap(new HashMap<>());
 
     public GameController(GameEngine gameEngine, EventAdapter eventAdapter) {
         this.gameEngine = gameEngine;
@@ -28,11 +32,13 @@ public class GameController extends TextWebSocketHandler implements GameObserver
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.put(session.getId(), session);
         gameEngine.registerPlayer(new Player(session.getId()));
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session.getId(), session);
         gameEngine.removePlayer(new Player(session.getId()));
     }
 
@@ -52,6 +58,11 @@ public class GameController extends TextWebSocketHandler implements GameObserver
     }
 
     private void notifyPlayer(PlayerNotification playerNotification) {
-        //todo
+        WebSocketSession session = sessions.get(playerNotification.getPlayer().getId());
+        try {
+            session.sendMessage(new TextMessage(playerNotification.getMessage()));
+        } catch (IOException e) {
+            log.error("Failed to send notification: {}", playerNotification, e);
+        }
     }
 }
